00eb0ef3e05953ea3183414af17bc8fd20b7fc70
Daron Adkins <daron.adkins@gmail.com>
2018-07-30 15:32:35 -0500

    geocoder working

--- app/Listing.php
+++ app/Listing.php
@@ -2,15 +2,23 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
+    use Searchable;
+
     protected $guarded = [];
     protected $with    = ['mediaObjects'];
 
     public function mediaObjects()
     {
         return $this->hasMany(MediaObject::class);
+    }
+
+    public function location()
+    {
+        return $this->hasOne(Location::class);
     }
 }
