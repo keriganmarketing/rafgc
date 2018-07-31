1021639942ecf1a6209b322f80e214fe5d273797
Daron Adkins <daron.adkins@gmail.com>
2018-07-31 13:31:52 -0500

    wip

---
+++ app/Http/Controllers/FeaturedListingsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Transformers\ListingTransformer;
+
+class FeaturedListingsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $mlsNumbers = explode('|', $request->mlsNumbers);
+
+        return fractal(
+            Listing::whereIn('mls_acct', $mlsNumbers)->get(),
+            new ListingTransformer
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Route::prefix('/api/v1')->group(function () {
     Route::get('search', 'ListingsSearchController@index')->name('listings.search');
+    Route::get('listings', 'FeaturedListingsController@index')->name('listings.featured');
 });
