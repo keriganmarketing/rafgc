899a3e2ab1ccc643864a8993f9e41144a3f8f826
Daron Adkins <daron.adkins@gmail.com>
2018-09-21 17:23:56 -0500

    wip

--- app/Transformers/MapSearchTransformer.php
+++ app/Transformers/MapSearchTransformer.php
@@ -27,7 +27,7 @@ class MapSearchTransformer extends TransformerAbstract
             'long'           => $listing->long,
             'lot_dimensions' => $listing->lot_dimensions,
             'mls_acct'       => $listing->mls_acct,
-            /* 'photo_url'      => $listing->url, */
+            'photo_url'      => $listing->url,
             'price'          => (int) $listing->list_price,
             'property_type'  => $listing->prop_type,
             'state'          => $listing->state,
